f4b47e6e7d7fdaca66d6373bdb1fbc6d071bcfe6
avadhutmali <mali.avadhut7@gmail.com>
2026-05-07 17:22:04 +0530

    feat:initilization

---
+++ aegis-server/aegis-server/src/test/java/com/aegis/aegis_server/AegisServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aegis.aegis_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
